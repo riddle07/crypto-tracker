4b125872107ad51e1578a3930e62e46c0a7d9671
Maaz <maazshuaib18@gmail.com>
2022-06-25 16:46:24 +0530

    Basic UI

---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+const Homepage = () => {
+  return (
+    <div>
+      <Banner/>
+    </div>
+  )
+}
+
+export default Homepage
