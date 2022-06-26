51b4d1a3ef20b8b5e093edba9a7900d574eed4df
Maaz <maazshuaib18@gmail.com>
2022-06-26 13:52:49 +0530

    Table Added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
+import CoinsTable from '../components/CoinsTable'
 const Homepage = () => {
   return (
     <div>
       <Banner/>
+      <CoinsTable/>
     </div>
   )
 }
