82b4412faa480fb5db48c23876c8f4646adc3882
Maaz <maazshuaib18@gmail.com>
2022-07-01 19:36:33 +0530

    Graph change with Time added

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -1,6 +1,5 @@
 import { makeStyles } from "@material-ui/core";
-
-const SelectButton = ({ children, selected, onClick }) => {
+import React from "react";
   const useStyles = makeStyles({
     selectbutton: {
       border: "1px solid gold",
@@ -10,9 +9,9 @@ const SelectButton = ({ children, selected, onClick }) => {
       paddingRight: 20,
       fontFamily: "Montserrat",
       cursor: "pointer",
-      backgroundColor: selected ? "gold" : "",
-      color: selected ? "black" : "",
-      fontWeight: selected ? 700 : 500,
+      backgroundColor: "gold",
+      color: "",
+      fontWeight: 500,
       "&:hover": {
         backgroundColor: "gold",
         color: "black",
@@ -21,7 +20,7 @@ const SelectButton = ({ children, selected, onClick }) => {
       //   margin: 5,
     },
   });
-
+const SelectButton = ({ children, selected, onClick }) => {
   const classes = useStyles();
 
   return (
